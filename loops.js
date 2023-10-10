019c9de9e2144d15c6d0bbb9c14c4944239f71a7
TubaGuneri <t_guneri@hotmail.com>
2023-10-10 15:34:05 +0200

    Initial

--- loops.js
+++ loops.js
@@ -11,7 +11,9 @@
 // Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN-artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
-
+for (let i = 0; i <  ; i++) {
+    
+}
 
 
 
